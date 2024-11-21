3d576cd6ae29ba4d0e580f295d698474a7ade1d7
pabloricardoo <145395553+pabloricardoo@users.noreply.github.com>
2024-11-21 13:57:20 -0300

    21/11

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'montanha'
-    'qual a montanha mais alta do brasil? '
+    'montanha',
+    'qual a montanha mais alta do brasil? ',
     'pico da Bandeira'
+)
 
 criaCartao(
-    'quimica'
-    'Atualmente, quantos elementos químicos a tabela periódica possui?
+    'quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui? ,
     'resposta'118
 )
 
 criaCartao(
-    'corpo'
-    'Quantos ossos temos no nosso corpo?
+    'corpo',
+    'Quantos ossos temos no nosso corpo?,
     'resposta'206
 )
 
 criaCartao(
-    'planeta'
-    'Qual o maior planeta do sistema solar?'
+    'planeta',
+    'Qual o maior planeta do sistema solar?',
     'Júpiter'
 )
 
